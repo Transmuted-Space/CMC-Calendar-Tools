cedc53852258954d34552ea55e37049331563bd7
Transmuted-Space <527253688543@protonmail.com>
2020-12-04 11:31:12 -0700

    Added parsing of calendar for events links and initial functionality for events parsing.

--- domains/calendar/cmc-calendar/calendar_injest.py
+++ domains/calendar/cmc-calendar/calendar_injest.py
@@ -9,6 +9,32 @@ from configparser import ConfigParser
 from html.parser import HTMLParser
 
 
+class EventHTMLParser(HTMLParser):
+    pass
+
+
+class CalendarHTMLParser(HTMLParser):
+
+
+    def __init__(self):
+        super().__init__()
+        self.event_links = []
+
+
+    def get_event_links(self):
+        return self.event_links
+
+
+    def handle_starttag(self, tag, attrs):
+
+        if tag == 'a':
+            attr_dict = dict(attrs)
+
+            if 'class' in attr_dict and attr_dict['class'] == 'calendarDayLink' and 'href' in attr_dict:
+                    self.event_links.append(attr_dict['href'])
+
+
+
 class CalendarInjest:
     '''
     Class encapsulation and functionality for interacting with the calendar API.
@@ -24,7 +50,7 @@ class CalendarInjest:
         self.config_parser.read('configuration.ini')
 
         # Instanciate HTML parser and logger.
-        self.html_parser = HTMLParser()
+        self.html_parser = CalendarHTMLParser()
 
 
     def export_calendar(self):
@@ -42,6 +68,13 @@ class CalendarInjest:
         pass
 
 
+    def injset_event_links(self, links):
+        for link in links:
+
+            # Send HTTP request to URI.
+            response = requests.get(calendarUri + link)
+
+
     def injest_calendar(self):
         '''
         This function will retrieve an HTML response from the URI and parse the DOM.
@@ -54,8 +87,15 @@ class CalendarInjest:
 
         # Handle depending on the response code.
         if response.status_code == 200:
+
+            # Feed the response HTML into the parser and parse.
             self.html_parser.feed(response.text)
+
+            # Retrieve events list and parse them all.
+            event_links = self.html_parser.get_event_links()
+            self.injest_event_links(event_links)
+
         elif response.status_code == 404:
-            logging.error('URI not found.')
+            logging.error('URI not found: HTTP status code {}.'.format(response.status_code))
         else:
-            logging.error('Unexpected response.')
+            logging.error('Unexpected response: HTTP status code {}.'.format(response.status_code))
--- domains/calendar/cmc-calendar/events.py
+++ domains/calendar/cmc-calendar/events.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.8
 
-from sqlalchemy import Integer, String, Datetime, Float
+from sqlalchemy import Integer, String, Text Datetime, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declaritive_base()
@@ -9,12 +9,20 @@ class Events(Base):
     __tablename__ = 'events'
 
     id = Column(Integer, primary_key=True)
-    datetime = Column(Datetime)
-    event_name = Column(String)
+    group = Column(String)
+    event_title = Column(String)
+    start_time = Column(Datetime)
+    end_time = Column(Datetime)
     status = Column(String)
     leader = Column(String)
     co_leader = Column(String)
+    member_price = Column(Float)
+    non_member_price = Column(Float)
     group_price = Column(Float)
     non_group_price = Column(Float)
     available_participants = Column(Integer)
     event_type = Column(String)
+    location = Column(Text)
+    contact_email = Column(String)
+    details = Column(Text)
+    registration_link = Column(String)
